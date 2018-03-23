d68c36a18b14e60f119b6a931bf435103c46e806
cexbrayat <cedric@ninja-squad.com>
2018-03-23 17:33:13 +0100

    feat: interval counter and test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,25 @@
-import { TestBed, async } from '@angular/core/testing';
+import { fakeAsync, tick, discardPeriodicTasks } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+
 describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  }));
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+
+  it('should update title every second', fakeAsync(() => {
+    const component = new AppComponent();
+
+    expect(component.title).toBe(-1);
+
+    // start the observable
+    component.ngOnInit();
+
+    tick(1000);
+    expect(component.title).toBe(0);
+
+    tick(1000);
+    expect(component.title).toBe(1);
+
+    tick(1000);
+    expect(component.title).toBe(2);
+
+    discardPeriodicTasks();
   }));
 });
